7e33f8be5a41b478353f5b134f7e26a885b7fbb4
Kazi Rahman -X (kazrahma - TATA CONSULTANCY SERVICES LIMITED at Cisco) <kazrahma@cisco.com>
2018-03-09 12:08:10 -0800

    finished node class

--- LinkedList.java
+++ LinkedList.java
@@ -1,9 +1,32 @@
-import java.util.*
 
-class LinkedList{
+class Node{
+	
+	private int data;
+	private Node next;
+	
+	public Node(){
+		this.data = 0;
+		this.next = null;
+	}
+	
+	public Node(int x){
+		this.data = x;
+		this.next = null;
+	}
+	
+	public int getData(){
+		return n.data;
+	}
+	 public void setData(int n){
+		 this.data = n;
+	 }
 
-	public 
-
+	public Node getNext(){
+		return n.next;
+	}
+	 public void setNext(Node n){
+		 this.data = n;
+	 }
 
 
 }
